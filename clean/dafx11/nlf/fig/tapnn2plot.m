b08cf637ab4821d2c9f0200b143f0891548ac691
julius.smith <julius.smith@8aa24caf-41f3-ad3a-9921-316658c278d0>
2011-03-26 03:29:24 +0000

    revised RMS level figure in nlf.tex

---
+++ clean/dafx11/nlf/fig/tapnn2plot.m
@@ -0,0 +1,11 @@
+set (0, "defaultaxesfontname", "Helvetica")
+set (0, "defaultaxesfontsize", 20)
+set (0, "defaulttextfontname", "Helvetica")
+set (0, "defaulttextfontsize", 24)
+grid('on');
+plot(faustout);
+title('RMS Level vs. Time');
+xlabel('Time (samples)');
+grid('on');
+[len,chans] = size(faustout);
+axis([0 len-1 0 1.0]);
